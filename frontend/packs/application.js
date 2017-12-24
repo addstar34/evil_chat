6f0ffa30e17e22c1e306b83909ee23312d0c8cd0
addstar34 <addstar34@gmail.com>
2017-12-24 20:19:31 +1000

    part 2 complete

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -1,3 +1,4 @@
-import "./application.css";
-
-document.body.insertAdjacentHTML("afterbegin", "Bla bla bla!");
+import "init";
+import "components/page/page";
+import "components/auth-form/auth-form";
+import "components/chat/chat";
